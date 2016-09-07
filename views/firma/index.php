9fab2b52b226649962064e724867b4921fb23d5d
luismelendez2 <luis.melendez2@upr.edu>
2016-09-07 10:30:42 -0400

    firmas
    cambio en firmas

--- views/firma/index.php
+++ views/firma/index.php
@@ -11,28 +11,40 @@ $this->title = 'Firmas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="firma-index">
+  <div class="col-sm-12">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1 align ="center">Firmas</h1>
+      </div>
+      <div class="panel-body">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+        <p>
+            <?= Html::a('Create Firma', ['create'], ['class' => 'btn btn-success']) ?>
+        </p>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
 
-    <p>
-        <?= Html::a('Create Firma', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                'id',
+                'id_supervisor',
+                'id_certificacion',
+                'firma',
+                'fecha_firma',
+                // 'created_at',
+                // 'updated_at',
 
-            'id',
-            'id_supervisor',
-            'id_certificacion',
-            'firma',
-            'fecha_firma',
-            // 'created_at',
-            // 'updated_at',
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+        <button class="btn btn-default" type="submit">Aceptar</button>
+        <button class="btn btn-default" type="submit">Rechazar</button>
+
+      </div>
+    </div>
+  </div>
 </div>
