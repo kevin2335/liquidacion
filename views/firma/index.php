5958bd23e5ac38a8776be389fd8594b765d8bfc2
luismelendez2 <luis.melendez2@upr.edu>
2016-09-08 16:02:31 -0400

    sencillo

--- views/firma/index.php
+++ views/firma/index.php
@@ -14,16 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
   <div class="panel panel-default">
       <div class="panel-heading">
         <h1 align ="center">Firmar</h1>
-<<<<<<< HEAD
-=======
 
-        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-
-
-
-    </div>
->>>>>>> refs/remotes/origin/master
 
       </div>
       <div class="panel-body">
@@ -39,43 +30,3 @@ $this->params['breadcrumbs'][] = $this->title;
       </div>
     </div>
   </div>
-<<<<<<< HEAD
-=======
-  <!-- <p>
-      <?= Html::a('Create Pregunta', ['create'], ['class' => 'btn btn-success']) ?>
-  </p> -->
-  <div class="firma-index">
-  <div class="row">
-      <div class="col-sm-6">
-        <div class="panel panel-defaul">
-                  <h1 align ="center"<button class="btn btn-default" type="submit">Aceptar</button>
-                    <h1 align ="center"<button class="btn btn-default" type="submit">Rechazar</button>
-              </div>
-            </div>
-
-    <!-- <h1><?= Html::encode($this->title) ?></h1>//antes
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Firma', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'id_supervisor',
-            'id_certificacion',
-            'firma',
-            'fecha_firma',
-            'created_at:date',
-            'updated_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-    <!--  -->
->>>>>>> refs/remotes/origin/master
-</div>
