5c4ca4caf9bed9cbfaf74f1af5d9817723f0c866
luismelendez2 <luis.melendez2@upr.edu>
2016-09-08 15:33:34 -0400

    again

--- views/estado/index.php
+++ views/estado/index.php
@@ -28,14 +28,24 @@ $this->params['breadcrumbs'][] = $this->title;
 
       </div>
       <div class="panel-footer">
+        <button class="btn btn-default" type="submit"><a href="index.php?r=resultado%2Findex">Ver Resultados</a></button>
       </div>
     </div>
   </div>
 
-            'id',
-            'estado',
+  <div class="col-sm-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1>Lista de Solicitudes</h1>
+      </div>
+      <div class="panel-body">
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+      </div>
+      <div class="panel-footer">
+        <button class="btn btn-default" type="submit"><a href="index.php?r=pregunta%2Findex">Firmar</a></button>
+      </div>
+    </div>
+  </div>
+
+
 </div>
