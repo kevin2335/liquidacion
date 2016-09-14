b6577a6aa01c2408cda2e34cd6fe79b698dea502
migChelly <michele.irizarry@upr.edu>
2016-09-14 10:30:26 -0400

    cambios en firma

--- views/firma/index.php
+++ views/firma/index.php
@@ -17,7 +17,7 @@ use yii\grid\GridView;
       <h1 align="center">Firmar</h1>
     </div>
     <div class="panel-body">
-      <h2> <small>"Certificar que la labor del funcionario esté al día y que haya entregado, registro de notas(si aplica), llaves u equipo y propiedad universitaria, entre otros."</small></h2>
+      <h2> <small>"Certificar que la labor del funcionario esté al día y que haya entregado, registro de notas(si aplica), llaves u equipo y propiedad universitaria y entre otros."</small></h2>
       </div>
       <div class="panel-footer">
         <a aling="center"><button class=" btn-default" type="submit">Firmar</button></a>
